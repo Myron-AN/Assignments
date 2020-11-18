a2c0cb0e9e8102e611a78c4e70a654a5392445cf
myron-an <myronstriker@gmail.com>
2020-11-18 23:40:54 +0500

    JS Assignment ch1 to ch2

---
+++ public/assets/assignments/javaScript/app.js
@@ -0,0 +1,118 @@
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+
+// Chapter1
+
+function AQ1() {
+  window.alert("Greetings!");
+}
+
+function AQ2() {
+  window.alert("Error! Please enter a valid password");
+}
+
+function AQ3() {
+  window.alert("Welcome to JS land..\nHappy Coding!");
+}
+
+function AQ4() {
+  window.alert("Welcome to JS Land...");
+  window.alert("Happy Coding!");
+}
+
+function AQ5() {
+  window.alert("Hello... I can run JS through my web browser's console");
+}
+
+
+// Chapter2
+
+
+function VS1() {
+
+  var username
+  console.log(username);
+
+}
+
+function VS2() {
+
+  var myName = "Myron";
+  console.log(myName);
+
+}
+
+function VS3() {
+
+  var message = "Hello World";
+  alert(message);
+
+}
+
+function VS4() {
+
+  var stName = "Jhone Doe";
+  var age = "15 years old";
+  var course = "Certified Mobile Application Development";
+  alert(stName);
+  alert(age);
+  alert(course);
+
+}
+
+function VS5() {
+
+  // var a = "Pizza";
+  // var b = "Pizz";
+  // var c = "Piz";
+  // var d = "Pi";
+  // var e = "P";
+  // alert(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e + "\n");
+
+  var food = "Pizza\nPizz\nPiz\nPi\nP";
+  alert(food);
+
+}
+
+function VS6() {
+
+  var email = "example@example.com"
+  alert("My eamil address is" + " " + email);
+
+}
+
+function VS7() {
+
+  var book = "A smarter way to learn JavaScript";
+  alert("I am trying to learn from the Book" + "\n" + book);
+
+}
+
+function VS8() {
+
+  var txt = "Yah! I can write HTML content through JavaScript";
+  document.write(txt);
+
+}
+
+function VS9() {
+
+  var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+  alert(a);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
