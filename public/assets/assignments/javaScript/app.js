9a5bca8c27dcd6e7c1395adfb58e2fc080f3e028
myron-an <myronstriker@gmail.com>
2020-11-19 22:08:16 +0500

    edited existing JS file

--- public/assets/assignments/javaScript/app.js
+++ public/assets/assignments/javaScript/app.js
@@ -32,87 +32,125 @@ function AQ5() {
 
 
 function VS1() {
-
   var username
   console.log(username);
-
 }
 
 function VS2() {
-
   var myName = "Myron";
   console.log(myName);
-
 }
 
 function VS3() {
-
   var message = "Hello World";
   alert(message);
-
 }
 
 function VS4() {
-
   var stName = "Jhone Doe";
   var age = "15 years old";
   var course = "Certified Mobile Application Development";
   alert(stName);
   alert(age);
   alert(course);
-
 }
 
 function VS5() {
-
-  // var a = "Pizza";
-  // var b = "Pizz";
-  // var c = "Piz";
-  // var d = "Pi";
-  // var e = "P";
-  // alert(a + "\n" + b + "\n" + c + "\n" + d + "\n" + e + "\n");
-
   var food = "Pizza\nPizz\nPiz\nPi\nP";
   alert(food);
-
 }
 
 function VS6() {
-
   var email = "example@example.com"
   alert("My eamil address is" + " " + email);
-
 }
 
 function VS7() {
-
   var book = "A smarter way to learn JavaScript";
   alert("I am trying to learn from the Book" + "\n" + book);
-
 }
 
 function VS8() {
-
   var txt = "Yah! I can write HTML content through JavaScript";
   document.write(txt);
-
 }
 
 function VS9() {
-
   var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
   alert(a);
-
 }
 
 
+// Chapter 3
+
+// var age = 24;
+// alert("i'm " + age + " years old");
+
+// var T = 15;
+// alert("you have visited this site " + T + " times");
 
 
+// var birthYear = 1996;
+// document.write("My birth year is " + birthYear + "<br>" + "Data type of my declared variable is number");
+
+// var visitorName = "John Doe";
+// var productTitile = "T-shirt(s)";
+// var qty = 5 ;
+// document.write(visitorName + " ordered " + qty + productTitile + " on XYZ Clothing store");
 
 
+// Chapter 4
+
+// var firstVariable = "Letters"; 
+// var secondVariable = "Numbers";
+// var thirdVariable = "Symbols";
+// document.write(firstVariable + " " + secondVariable + " " + thirdVariable);
+
+// // legal Variables
+// var $name;
+// var _name;
+// var firstName;
+// var myName0;
+// var $my_1stName;
+// // Illegal Varibale
+// var my name;
+// var alert;
+// var $&62##;
+// var 1st_name;
+// var my-name;
+
+// document.write("<strong>Rules for Naming JS variables!</strong>" + "<br><br>");
+// document.write("Variable names can only contain numbers, letters, $ and _. For example: $my_1stVariable. <br>");
+// document.write("Variable must begin with a letter, $ or _. For example: $name, _name or name. <br>");
+// document.write("Variable name are case sensitive. <br>");
+// document.write("Variable name should not be JS keywords. <br>");
 
 
+// Chapter 5
 
+// var a = 5;
+// var b = 5;
+// var c = a + b;
+// document.write("Sum of 5 and 5 is " + c + "<br>");
 
+// var d = 10;
+// var e = 5;
+// var f = d - e;
+// document.write("Subtraction of 10 and 5 is " + f + "<br>");
 
+// var g = 2;
+// var h = 2;
+// var i = g * h;
+// document.write("Multiplication of 2 and 2 is " + i + "<br>");
 
+// var j = 9;
+// var k = 3;
+// var l = j / k;
+// document.write("Division of 9 and 3 is " + l + "<br>");
+
+// var m = 8;
+// var n = 2;
+// var o = m % n;
+// document.write("Modulus of 8 and 2 is " + o + "<br>");
+
+var a = 5;
